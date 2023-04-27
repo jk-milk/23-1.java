24f845bee82c3f2d7bcccec09b0a95ef332fa0e5
jk <yjkeong@gmail.com>
2023-04-27 14:15:37 +0900

    4/27

---
+++ unit7/interfacetest/AdvancedRemoteControl.java
@@ -0,0 +1,6 @@
+package unit7.interfacetest;
+
+public interface AdvancedRemoteControl extends RemoteControl {
+  
+  void volumeControl(int amount);
+}
---
+++ unit7/interfacetest/RemoteControl.java
@@ -0,0 +1,13 @@
+package unit7.interfacetest;
+
+public interface RemoteControl {
+  void turnOn();
+  void turnOff();
+
+  /* default void volumeControl(int amount) {
+    String st = null;
+    st = amount >= 0 ? "높입니다." : "줄입니다.";
+    amount = Math.abs(amount); // |number|
+    System.out.println("볼륨을 " + amount + "만큼 " + st);
+  } */
+}
